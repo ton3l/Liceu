06b869a10777dfb293571f0a090807b0a09fd723
Elton <elton.felype.s.nascimento@gmail.com>
2025-05-31 17:23:27 -0300

    Modularização básica, aplicando gh-pages.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/svgs/logo.svg';
+import './styles/App.css';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
